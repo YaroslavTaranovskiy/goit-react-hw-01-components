8fad3308757714201d095c255ef6f3f22797bd9c
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2023-02-24 13:19:56 +0200

    start

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,28 +1,12 @@
-import PropTypes from 'prop-types';
 import styles from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
 
 const FriendList = ({ friends }) => {
-  return (
-    <section className={styles.friendList}>
-      <ul className={styles.friend_list}>
-        {friends.map(friend => (
-          <li className={styles.item} key={friend.id}>
-            <span className={styles.item}></span>
-            <img
-              className={styles.avatar}
-              src={friend.avatar}
-              alt={friend.name}
-              width="48"
-            />
-            <p className={styles.name}>{friend.name}</p>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-};
-FriendList.propTypes = {
-  friends: PropTypes.array.isRequired,
+  <ul className={styles.frend_list}>
+    {friends.map(friend => {
+      return <FriendListItem frend={friend} key={friend.id} />;
+    })}
+  </ul>;
 };
 
 export default FriendList;

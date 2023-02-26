f1a31cc2b18333f6dddbaea0003aa34dcb99b5ae
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2023-02-26 14:52:50 +0200

    start

--- src/components/friendList/FriendList.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const FriendList = styled.ul`
-    display: grid;
-    grid-template-columns: 1fr;
-    gap: ${props => props.theme.spacing[3]}px;
-
-    margin: ${props => props.theme.spacing[0]} auto;
-`;
